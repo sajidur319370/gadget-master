1bd6ea94f94288ee8cf19e0dcaf160e706d506f0
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-05-15 04:37:43 +0600

    responsive done

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Getting Started with Gadget Master
 
 site link:https://gadget-master-c9a7a.firebaseapp.com/
+stack overflow question-answer link:https://stackoverflow.com/questions/72244216/why-does-my-chrome-browser-show-the-issue-audit-usage-of-navigator-useragent-n
 
 In the project I Use
 
--- src/Components/Pages/Shared/Footer/Footer.js
+++ src/Components/Pages/Shared/Footer/Footer.js
@@ -1,58 +1,83 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
 
   return (
-    <div className="bg-dark py-5 ">
+    <div className="bg-dark pt-5 ">
       <h2>
         <span style={{ color: "#fd7e14" }}>Gadget Master</span>
       </h2>
-      <div className="d-flex text-white align-items-center justify-content-center">
-        <ul style={{ listStyle: "none", padding: "20px" }}>
-          <li>
-            <p className="text-secondary fs-3">Category</p>
-          </li>
-          <li>
-            <h6>Laptop</h6>
-          </li>
-          <li>
-            <h6>Camera</h6>
-          </li>
-          <li>
-            <h6>Router</h6>
-          </li>
-        </ul>
-        <ul style={{ listStyle: "none", padding: "20px" }}>
-          <li>
-            <p className="text-secondary fs-3">Customers</p>
-          </li>
-          <li>
-            <h6>Local People</h6>
-          </li>
-          <li>
-            <h6>Big City People</h6>
-          </li>
-          <li>
-            <h6>Foreign People</h6>
-          </li>
-        </ul>
-        <ul style={{ listStyle: "none", padding: "20px" }}>
-          <li>
-            <p className="text-secondary fs-3">Company</p>
-          </li>
-          <li>
-            <h6>Samsung</h6>
-          </li>
-          <li>
-            <h6>Cannon</h6>
-          </li>
-          <li>
-            <h6>TP-link</h6>
-          </li>
-        </ul>
-      </div>
+      <Row className="text-secondary ">
+        <Col sm={12} md={6} lg={3}>
+          <ul style={{ listStyle: "none", padding: "20px" }}>
+            <li>
+              <p className="text-white fs-3">Category</p>
+            </li>
+            <li>
+              <h6>Laptop</h6>
+            </li>
+            <li>
+              <h6>Camera</h6>
+            </li>
+            <li>
+              <h6>Router</h6>
+            </li>
+          </ul>
+        </Col>
+        <Col sm={12} md={6} lg={3}>
+          <ul style={{ listStyle: "none", padding: "20px" }}>
+            <li>
+              <p className="text-white fs-3">Customers</p>
+            </li>
+            <li>
+              <h6>Local People</h6>
+            </li>
+            <li>
+              <h6>Big City People</h6>
+            </li>
+            <li>
+              <h6>Foreign People</h6>
+            </li>
+          </ul>
+        </Col>
+
+        <Col sm={12} md={6} lg={3}>
+          <ul style={{ listStyle: "none", padding: "20px" }}>
+            <li>
+              <p className="text-white fs-3">Company</p>
+            </li>
+            <li>
+              <h6>Samsung</h6>
+            </li>
+            <li>
+              <h6>Cannon</h6>
+            </li>
+            <li>
+              <h6>TP-link</h6>
+            </li>
+          </ul>
+        </Col>
+        <Col sm={12} md={6} lg={3}>
+          <ul style={{ listStyle: "none", padding: "20px" }}>
+            <li>
+              <p className="text-white fs-3">Address</p>
+            </li>
+            <li>
+              <h6>Elephant Road,Dhaka,1207</h6>
+            </li>
+            <li>
+              <h6>Contact:0155555555</h6>
+            </li>
+            <li>
+              <Link to="/">abc@def.com</Link>
+            </li>
+          </ul>
+        </Col>
+      </Row>
       <p className="fs-6 lead text-white bg-secondary py-3 my-0">
         copyright &copy; <small>{year}</small> by {""}
         <span style={{ color: "#fd7e14", fontWeight: "600" }}>
